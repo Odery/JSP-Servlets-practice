5af2f53c616ca0ada7107d9dc7443b262d0deeed
Vakamisu <kammurimini@gmail.com>
2018-03-14 21:34:13 +0200

    +added MVC demo

---
+++ src/servlets/Controller.java
@@ -0,0 +1,20 @@
+package servlets;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet("/controller")
+public class Controller extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String[] model = {"Steve", "Igor", "Roger", "Henry"};
+        request.setAttribute("list", model);
+
+        RequestDispatcher dispatcher = request.getRequestDispatcher("/view.jsp");
+    }
+}
